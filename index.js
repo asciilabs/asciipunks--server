b6ed9f7c2547e4506ec6d287446dd61ea2eac0f7
kyle <kylelholzinger@gmail.com>
2021-03-20 17:10:14 -0400

    [KH] embed real token in response

--- index.js
+++ index.js
@@ -1,11 +1,43 @@
+require('dotenv').config()
 const express = require('express')
 const app = express()
 
-app.get('/punks/:id', (req, res) => {
+const Web3 = require('web3')
+
+const web3 = new Web3(
+  new Web3.providers.HttpProvider(process.env.ETH_HTTP_PROVIDER_URL)
+)
+
+const contractAddress = '0xd4e4078ca3495de5b1d4db434bebc5a986197782'
+const contract = new web3.eth.Contract(
+  [
+    {
+      type: 'function',
+      name: 'tokenURI',
+      inputs: [{ name: '_tokenId', type: 'uint256' }],
+      outputs: [{ name: 'autoglyph', type: 'string' }],
+    },
+
+    {
+      type: 'function',
+      name: 'draw',
+      inputs: [{ name: '_tokenId', type: 'uint256' }],
+      outputs: [{ name: 'autoglyph', type: 'string' }],
+    },
+  ],
+  contractAddress
+)
+
+const renderSvg = async (id) => {
+  const token = await contract.methods.tokenURI(id).call()
+
+  return `<svg xmlns="http://www.w3.org/2000/svg" width="350" height="350"><text x="50%" y="50%" dominant-baseline="middle" text-anchor="middle" font-size="75px" fill="#fffffa">${token}</text></svg>`
+}
+
+app.get('/punks/:id', async (req, res) => {
   const id = req.params.id
   res.send({
-    image_data:
-      '<svg xmlns="http://www.w3.org/2000/svg" width="350" height="350"><text x="50%" y="50%" dominant-baseline="middle" text-anchor="middle" font-size="75px" fill="#fffffa">(▀̿Ĺ̯▀̿ ̿)</text></svg>',
+    image_data: await renderSvg(1),
     description: '',
     name: `ASCII Punk #${id}`,
     attributes: [],
